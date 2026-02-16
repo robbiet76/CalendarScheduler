d418eedde10548eb98b13e82d668e30f524a513f
Rob Terry <rob.terry@live.com>
2026-02-15 23:30:55 -0500

    Remove obsolete archive directory

--- archive/v2/src/Resolution/EventResolver.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-
-namespace GoogleCalendarScheduler\Resolution;
-
-interface EventResolver
-{
-    public function resolve(ResolutionInputs $inputs): ResolutionResult;
-}
