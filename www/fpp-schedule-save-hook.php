9e939f793630c6856d073171eeb5656ec65c8769
Rob Terry <rob.terry@live.com>
2026-02-17 10:41:06 -0500

    Remove duplicate schedule-save hook wrapper from www

--- www/fpp-schedule-save-hook.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-// FPP plugin.php does NOT resolve pages from www/.
-// Keep this file as a wrapper for any direct access paths.
-require_once __DIR__ . '/../fpp-schedule-save-hook.php';
